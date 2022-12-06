7164643fe2c5c42fe84ff9e81fbac779599a976d
Migle Urbonaite <mig.urbonaite@gmail.com>
2022-12-06 17:09:40 +0100

    Added ClingingStairs solution

---
+++ climbing-stairs/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+public class Solution
+{
+	public int ClimbStairs (int n)
+	{
+		Dictionary<int, int> dict = new Dictionary<int, int>();
+		if ( n < 0 )
+			return 0;
+		if ( n == 0 || n == 1 )
+			return 1;
+		if ( dict.ContainsKey(n) )
+			return dict[n];
+		dict[n] = ClimbStairs(n - 1) + ClimbStairs(n - 2);
+		return dict[n];
+	}
+
+}
