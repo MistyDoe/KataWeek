6a98f14550513a1f8f4f48e9f65c232ac14a5ae8
Migle Urbonaite <mig.urbonaite@gmail.com>
2022-12-14 17:41:42 +0100

    fixed read me

--- Solving Questions With Brainpower/Program.cs
+++ Solving Questions With Brainpower/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+internal class Program
+{
+	private static void Main (string[] args)
+	{
+		long MostPoints (int[][] questions)
+		{
+
+			var points = new long[questions.Length];
+			points[points.Length - 1] = questions[questions.Length - 1][0];
+
+			for ( int i = questions.Length - 2; i >= 0; i-- )
+			{
+
+				int nextQuestion = questions[i][1] + i + 1;
+
+				long possiblePoints = questions[i][0];
+				if ( nextQuestion < questions.Length )
+					possiblePoints += points[nextQuestion];
+
+				points[i] = Math.Max(possiblePoints, points[i + 1]);
+			}
+
+			return points[0];
+		}
+	}
+}
